05cdc213094fe9bbf6d6f4460c1dd61695f83561
Greg Magarshak <greg@qbix.com>
2026-01-14 10:57:56 -0500

    Refactored migration script to use nextChunk

--- scripts/Calendars/0.4-Streams.mysql.php
+++ scripts/Calendars/0.4-Streams.mysql.php
@@ -1,62 +1,89 @@
 <?php
-	
+
 function Calendars_0_4_Streams()
 {
 	// first community
-	$communities = array(Users::communityId() => Users::communityName());
+	$communities = array(
+		Users::communityId() => Users::communityName()
+	);
 	// other communities
-	$communities = array_merge($communities, Q_Config::get('Communities', 'communities', array()));
+	$communities = array_merge(
+		$communities,
+		Q_Config::get('Communities', 'communities', array())
+	);
 
 	foreach ($communities as $communityId => $communityName) {
+
 		$streamName = 'Calendars/calendar/main';
 
 		// get or create stream
 		$stream = Streams_Stream::fetch($communityId, $communityId, $streamName);
 		if (!($stream instanceof Streams_Stream)) {
-			$stream = Streams::create($communityId, $communityId, 'Calendars/calendar', array(
-				'name' => $streamName
-			));
+			$stream = Streams::create(
+				$communityId,
+				$communityId,
+				'Calendars/calendar',
+				array('name' => $streamName)
+			);
 		}
 
 		if (!($stream instanceof Streams_Stream)) {
-			die("[ERROR]: wrong events main category for $communityId".PHP_EOL);
+			die("[ERROR]: wrong events main category for $communityId" . PHP_EOL);
 		}
 
-		echo "Joining registered users to {$stream->publisherId} {$stream->name}".PHP_EOL;
-		$offset = 0;
-		$i = 0;
-		while (1) {
-			$users = Users_User::select('u.*', 'u')
-				->join(
-					'streams_participant sp',
-					array(
-						'sp.userId' => new Db_Expression('u.id'),
-						'sp.publisherId' => '?',
-						'sp.streamName' => '?' 
-					),
-					'LEFT'
-				)
-				->where(array(
-					'u.signedUpWith !=' => 'none',
-					'sp.userId' => null // NOT participating
-				))
-				->limit(100, $offset)
-				->bind(array($stream->publisherId, $stream->name))
-				->fetchDbRows();
-			if (!$users) {
-				break;
-			}
-			foreach ($users as $user) {
+		echo "Joining registered users to {$stream->publisherId} {$stream->name}" . PHP_EOL;
 
-				$stream->join(array('userId' => $user->id));
+		$i = 0;
+
+		$q = Users_User::select('u.*', 'u')
+			->join(
+				'streams_participant sp',
+				array(
+					'sp.userId' => 'u.id',
+					'sp.publisherId' => '?',
+					'sp.streamName' => '?'
+				),
+				'LEFT'
+			)
+			->where(array(
+				'u.signedUpWith !=' => 'none',
+				'sp.userId' => null // NOT participating
+			))
+			->orderBy('u.id', true)
+			->bind(array(
+				$stream->publisherId,
+				$stream->name
+			))
+			->nextChunk(array(
+				'chunkSize' => 100,
+				'index' => 'u.id'
+			));
+
+		$users = $q->fetchDbRows(null, 'u_', 'id');
+
+		while ($users) {
+
+			foreach ($users as $userId => $user) {
+
+				$stream->join(array(
+					'userId' => $user->id
+				));
 
 				++$i;
 				echo "\033[100D";
 				echo "Joined $i users";
 			}
-			$offset += 100;
+
+			// advance cursor (keys are ids)
+			end($users);
+			$q->lastChunkValue = key($users);
+
+			$q->nextChunk();
+			$users = $q->fetchDbRows(null, 'u_', 'id');
 		}
+
 		echo PHP_EOL;
 	}
 }
+
 Calendars_0_4_Streams();
