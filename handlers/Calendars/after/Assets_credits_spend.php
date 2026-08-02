4ace976cf8b70cc7d19b6cf3e7d404c72c54e79d
Greg Magarshak <greg@qbix.com>
2026-08-02 15:28:57 -0400

    Improved getPaymentsInfo to consult referrerUserId in webhooks

--- handlers/Calendars/after/Assets_credits_spend.php
+++ handlers/Calendars/after/Assets_credits_spend.php
@@ -6,16 +6,19 @@ function Calendars_after_Assets_credits_spend($params)
     $fromUserId = Q::ifset($params, 'fromUserId', null);
     $toPublisherId = Q::ifset($params, 'options', 'toPublisherId', null);
     $toStreamName = Q::ifset($params, 'options', 'toStreamName', null);
+    $intent = Q::ifset($params, 'options', 'intent', null);
+    $referrerUserId = $intent ? $intent->getInstruction('referrerUserId') : null;
 
     if (!Q::startsWith($toStreamName, 'Calendars/event/') || $amountCredits <= 0) {
         return;
     }
 
     $stream = Streams_Stream::fetch($toPublisherId, $toPublisherId, $toStreamName, true);
-    if (Assets_Credits::getPaymentsInfo($fromUserId, array(
+    $paymentsInfo = Assets_Credits::getPaymentsInfo($fromUserId, array(
         'publisherId' => $toPublisherId,
         'streamName'  => $toStreamName
-    ))["conclusion"]["fullyPaid"]) {
+    ), null, $referrerUserId);
+    if ($paymentsInfo["conclusion"]["fullyPaid"]) {
         // TODO: check whether event requires approval,
         // and whether the user has been approved.
         // For now, just let them attend the event and see its location.
@@ -41,6 +44,6 @@ function Calendars_after_Assets_credits_spend($params)
     }
 
     Calendars_Event::going($stream, $fromUserId, $going, array(
-        'skipPayment' => true // avoid infinite
+        'skipPayment' => true // avoid infinite recursion
     ));
 }
